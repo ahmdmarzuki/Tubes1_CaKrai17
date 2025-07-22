9c8ea34b242d9a0d30757439b65df1ac55a699cf
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-07-22 20:28:39 +0700

    fixing

--- main.cpp
+++ main.cpp
@@ -3,13 +3,8 @@
 using namespace std;
 
 int main() {
-    cout << "Program dimulai!" << endl; // Pastikan ini muncul
-
     FSM robotFSM(2000);
     robotFSM.start();
     
-    cout << "Program selesai" << endl; // Pastikan ini muncul
-    
-    system("pause");
     return 0;
 }
